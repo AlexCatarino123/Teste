8703120210bec86953f51f3147b900f56c057d27
Alexandre <Alexandre@Catarino>
2015-09-08 13:36:43 -0300

    Mudanças 1

--- TesteGit/src/br/unifor/teste/br/Testando.java
+++ TesteGit/src/br/unifor/teste/br/Testando.java
@@ -3,7 +3,7 @@ package br.unifor.teste.br;
 public class Testando {
 
 	public static void main(String[] args) {
-		System.out.println("Oi mundo!!");
+		System.out.println("Oi mundo Corrigido!!!");
 
 	}
 
